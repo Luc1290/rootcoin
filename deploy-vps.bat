3a9efa0a3ea701a86d75aa2629aeb06e809abec5
Luc <Lucparguel12720@gmail.com>
2026-02-21 00:38:54 +0100

    Root 0.0.9

--- deploy-vps.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo === Deploy RootCoin to VPS ===
-ssh root@100.83.87.98 "cd /home/rootcoin_app && git pull && source venv/bin/activate && pip install -r requirements.txt --quiet && sudo systemctl restart rootcoin && echo === Deploy complete ==="
-pause
